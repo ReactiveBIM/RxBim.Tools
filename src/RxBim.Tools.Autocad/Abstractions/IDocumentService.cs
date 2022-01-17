7abd903b19577b95f4337d60351897a279311403
Вильдар Хисяметдинов <vildar82@gmail.com>
2022-01-17 20:03:49 +0300

    Add IDocumentService, CommandLineService use it.

---
+++ src/RxBim.Tools.Autocad/Abstractions/IDocumentService.cs
@@ -0,0 +1,16 @@
+﻿namespace RxBim.Tools.Autocad.Abstractions
+{
+    using Autodesk.AutoCAD.ApplicationServices;
+    using CSharpFunctionalExtensions;
+
+    /// <summary>
+    /// Сервис документа
+    /// </summary>
+    public interface IDocumentService
+    {
+        /// <summary>
+        /// Возвращает текущий документ
+        /// </summary>
+        Result<Document> GetActiveDocument();
+    }
+}
