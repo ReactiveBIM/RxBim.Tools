cfe3643a4ef9e9a8b57a21e0160c0a48d8ff4f99
Ефрем Герасимов <gerasimovea@pik.ru>
2022-02-04 16:52:37 +0300

    fix TransactionService

--- src/RxBim.Tools.Revit/Abstractions/ITransactionService.cs
+++ src/RxBim.Tools.Revit/Abstractions/ITransactionService.cs
@@ -1,6 +1,7 @@
 ﻿namespace RxBim.Tools.Revit.Abstractions
 {
     using System;
+    using Autodesk.Revit.DB;
     using CSharpFunctionalExtensions;
 
     /// <summary>
@@ -13,27 +14,35 @@
         /// </summary>
         /// <param name="action">Действие</param>
         /// <param name="transactionName">Название транзакции</param>
-        Result RunInTransaction(Action action, string transactionName);
+        /// <param name="document">выбранный документ.
+        /// при null - транзакция запустится в текущем документе</param>
+        Result RunInTransaction(Action action, string transactionName, Document document = null);
 
         /// <summary>
         /// Оборачивает действие в группу транзакций
         /// </summary>
         /// <param name="action">Действие</param>
         /// <param name="transactionGroupName">Название группы транзакций</param>
-        Result RunInTransactionGroup(Action action, string transactionGroupName);
+        /// <param name="document">выбранный документ.
+        /// при null - транзакция запустится в текущем документе</param>
+        Result RunInTransactionGroup(Action action, string transactionGroupName, Document document = null);
 
         /// <summary>
         /// Оборачивает действие в транзакцию с возвратом результата
         /// </summary>
         /// <param name="action">Действие с возвратом результата</param>
         /// <param name="transactionName">Название транзакции</param>
-        Result RunInTransaction(Func<Result> action, string transactionName);
+        /// <param name="document">выбранный документ.
+        /// при null - транзакция запустится в текущем документе</param>
+        Result RunInTransaction(Func<Result> action, string transactionName, Document document = null);
 
         /// <summary>
         /// Оборачивает действие в группу транзакций с возвратом результата
         /// </summary>
         /// <param name="action">Действие с возвратом результата</param>
         /// <param name="transactionGroupName">Название группы транзакций</param>
-        Result RunInTransactionGroup(Func<Result> action, string transactionGroupName);
+        /// <param name="document">выбранный документ.
+        /// при null - транзакция запустится в текущем документе</param>
+        Result RunInTransactionGroup(Func<Result> action, string transactionGroupName, Document document = null);
     }
 }
