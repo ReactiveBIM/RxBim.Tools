f2e3b97628482b65db5c45ff42d8059fab990389
Вильдар Хисяметдинов <khisyametdinovvt@pik.ru>
2022-03-30 21:18:27 +0400

    fix test nullable

--- tests/RxBim.Tools.TableBuilder.Tests/TableBuildingTests.cs
+++ tests/RxBim.Tools.TableBuilder.Tests/TableBuildingTests.cs
@@ -285,7 +285,7 @@
         {
             public int Prop1 { get; set; }
 
-            public string Prop2 { get; set; }
+            public string Prop2 { get; set; } = string.Empty;
         }
     }
 }
