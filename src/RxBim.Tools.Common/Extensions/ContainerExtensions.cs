ccff9bb20de357011575df4b2cb2cc8d3545443b
Dmitriy Zagorulkin <zagorulkindiu@pik.ru>
2022-07-01 17:08:56 +0300

    some extensions

---
+++ src/RxBim.Tools.Common/Extensions/ContainerExtensions.cs
@@ -0,0 +1,73 @@
+﻿namespace RxBim.Tools.Common.Extensions
+{
+    using System;
+    using Di;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Extensions for <see cref="IContainer"/>.
+    /// </summary>
+    public static class ContainerExtensions
+    {
+        /// <summary>
+        /// Adds a transient object of the type specified in <typeparamref name="T"/> to the DI container.
+        /// The object data is loaded from the configuration.
+        /// </summary>
+        /// <param name="container">The instance of <see cref="IContainer"/>.</param>
+        /// <param name="config">Configuration. If null, the configuration is taken from the container.</param>
+        /// <param name="sectionName">
+        /// The name of the configuration section for the object data.
+        /// If null, the name of the type specified in <typeparamref name="T"/> is used.
+        /// </param>
+        /// <typeparam name="T">The type of the requested object.</typeparam>
+        public static IContainer AddTransientFromConfig<T>(
+            this IContainer container,
+            IConfiguration? config = null,
+            string? sectionName = null)
+            where T : class
+        {
+            return container.AddFromConfig<T>(true, config, sectionName);
+        }
+
+        /// <summary>
+        /// Adds a singleton object of the type specified in <typeparamref name="T"/> to the DI container.
+        /// The object data is loaded from the configuration.
+        /// </summary>
+        /// <param name="container">The instance of <see cref="IContainer"/>.</param>
+        /// <param name="config">Configuration. If null, the configuration is taken from the container.</param>
+        /// <param name="sectionName">
+        /// The name of the configuration section for the object data.
+        /// If null, the name of the type specified in <typeparamref name="T"/> is used.
+        /// </param>
+        /// <typeparam name="T">The type of the requested object.</typeparam>
+        public static IContainer AddSingletonFromConfig<T>(
+            this IContainer container,
+            IConfiguration? config = null,
+            string? sectionName = null)
+            where T : class
+        {
+            return container.AddFromConfig<T>(false, config, sectionName);
+        }
+
+        private static IContainer AddFromConfig<T>(
+            this IContainer container,
+            bool addAsTransient,
+            IConfiguration? config,
+            string? sectionName)
+            where T : class
+        {
+            var section = sectionName ?? typeof(T).Name;
+            var factory = config is null
+                ? (Func<T>)(() => container.GetService<IConfiguration>().GetSection(section).Get<T>())
+                : () => config.GetSection(section).Get<T>();
+
+            return container.Add(addAsTransient, factory);
+        }
+
+        private static IContainer Add<T>(this IContainer container, bool transient, Func<T> func)
+            where T : class
+        {
+            return transient ? container.AddTransient(func) : container.AddSingleton(func);
+        }
+    }
+}
---
+++ src/RxBim.Tools.Common/Extensions/EnumerationExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace RxBim.Tools.Common.Extensions
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+
+    /// <summary>
+    /// Extensions for enumerations.
+    /// </summary>
+    public static class EnumerationExtensions
+    {
+        /// <summary>
+        /// Returns a description for the enum value.
+        /// </summary>
+        /// <param name="value">Enum value.</param>
+        /// <typeparam name="T">Type of enum.</typeparam>
+        public static string GetEnumDescription<T>(this T value)
+            where T : Enum
+        {
+            var fi = value.GetType().GetField(value.ToString());
+
+            return fi.GetCustomAttributes(typeof(DescriptionAttribute), false) is DescriptionAttribute[] attributes &&
+                   attributes.Any()
+                ? attributes.First().Description
+                : value.ToString();
+        }
+    }
+}
