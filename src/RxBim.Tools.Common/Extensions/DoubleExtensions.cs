6fee09fe2fa18212a3628a2548d6805fc1c06113
Dmitriy Zagorulkin <zagorulkindiu@pik.ru>
2022-07-01 17:08:56 +0300

    some extensions

--- src/RxBim.Tools.Common/Extensions/DoubleExtensions.cs
+++ src/RxBim.Tools.Common/Extensions/DoubleExtensions.cs
@@ -9,6 +9,7 @@
     {
         private const double Epsilon = 1e-6;
         private const double DegreesInRadian = 180D / Math.PI;
+        private const int DefaultPrecisionDigits = 4;
 
         /// <summary>
         /// Returns true if a number is equal to another number with the given precision.
@@ -69,6 +70,21 @@
         public static double DegreesToRadians(this double degrees)
         {
             return degrees / DegreesInRadian;
+        }
+
+        /// <summary>
+        /// Rounds a double-precision floating-point value to a specified number of fractional digits.
+        /// </summary>
+        /// <param name="value">A double-precision floating-point number to be rounded.</param>
+        /// <param name="digits">The number of fractional digits in the return value.</param>
+        /// <returns>
+        /// The number nearest to value that has a number of fractional digits equal to digits.
+        /// If value has fewer fractional digits than digits, value is returned unchanged.
+        /// </returns>
+        /// <remarks>Internally, the System.Math.Round method is used with Mode=MidpointRounding.AwayFromZero.</remarks>
+        public static double Round(this double value, int digits = DefaultPrecisionDigits)
+        {
+            return Math.Round(value, digits, MidpointRounding.AwayFromZero);
         }
     }
 }
