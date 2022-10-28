2b777a08ecbf2a8cde1058ab31bfd4c5688b47b0
Вильдар Хисяметдинов <khisyametdinovvt@pik.ru>
2022-10-28 17:21:54 +0600

    tests

---
+++ tests/RxBim.Tools.Autocad.Tests/TransactionContainerRegistrationTests.cs
@@ -0,0 +1,29 @@
+﻿namespace RxBim.Tools.Autocad.Tests
+{
+    using Di;
+    using FluentAssertions;
+    using Moq;
+    using Xunit;
+
+    public class TransactionContainerRegistrationTests
+    {
+        [Fact]
+        public void DefaultContextRegistrationTest()
+        {
+            var mockDocumentContext = new Mock<ITransactionContextService<IDocumentWrapper>>();
+            var mockDatabaseContext = new Mock<ITransactionContextService<IDatabaseWrapper>>();
+
+            var di = new TestDiConfigurator();
+            di.Configure(GetType().Assembly);
+            var container = di.Container;
+
+            container.AddSingleton<ITransactionContextService<IDocumentWrapper>>(mockDocumentContext.Object);
+            container.AddSingleton<ITransactionContextService<IDatabaseWrapper>>(mockDatabaseContext.Object);
+            container.AddSingleton<ITransactionContextService<ITransactionContextWrapper>>(mockDocumentContext.Object);
+
+            var result = container.GetService<ITransactionContextService<ITransactionContextWrapper>>();
+
+            result.Should().Be(mockDocumentContext.Object);
+        }
+    }
+}
---
+++ tests/RxBim.Tools.Revit.Tests/Config/TestDiConfigurator.cs
@@ -0,0 +1,13 @@
+﻿namespace RxBim.Tools.Revit.Tests
+{
+    using Di;
+
+    public class TestDiConfigurator : DiConfigurator<IPluginConfiguration>
+    {
+        /// <inheritdoc />
+        protected override void ConfigureBaseDependencies()
+        {
+            // Container.AddTransient<IBaseService, BaseService>();
+        }
+    }
+}
