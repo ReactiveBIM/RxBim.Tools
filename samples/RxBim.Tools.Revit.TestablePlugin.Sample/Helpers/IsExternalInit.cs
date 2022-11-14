27a668b1ecabc87210100bd60de41a44250642a5
pavlovek <pavlovek@pik.ru>
2022-11-14 15:08:28 +0300

    update dependencies

--- samples/RxBim.Tools.Revit.TestablePlugin.Sample/Helpers/IsExternalInit.cs
+++ samples/RxBim.Tools.Revit.TestablePlugin.Sample/Helpers/IsExternalInit.cs
@@ -7,6 +7,7 @@ namespace System.Runtime.CompilerServices
     /// Reserved to be used by the compiler for tracking metadata.
     /// This class should not be used by developers in source code.
     /// </summary>
+    /// <remarks>https://stackoverflow.com/a/64749403</remarks>
     [EditorBrowsable(EditorBrowsableState.Never)]
     internal static class IsExternalInit 
     {
