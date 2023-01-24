0104ce841ee9b1f271ca8c6e9403a1c2ea7efe8c
Вильдар Хисяметдинов <khisyametdinovvt@pik.ru>
2023-01-24 15:58:36 +0600

    GetLocalConfig

---
+++ src/Core/RxBim.Tools/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,70 @@
+﻿namespace RxBim.Tools
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Assembly Extensions.
+    /// </summary>
+    public static class AssemblyExtensions
+    {
+        private static readonly Regex AssemblyNameVersionRex = new(
+            @"(?<version>\.\d+\.\d+\.\d+(\.\d+)?)?$",
+            RegexOptions.Singleline | RegexOptions.CultureInvariant);
+
+        /// <summary>
+        /// Returns the configuration for the build from the appsettings.[build name].json
+        /// file and additional JSON files (if any).
+        /// </summary>
+        /// <param name="assembly">Assembly.</param>
+        /// <param name="additionalJsonFileNamePattern">Name template for additional uploaded json files.</param>
+        public static IConfiguration GetLocalConfig(
+            this Assembly assembly,
+            string? additionalJsonFileNamePattern = null)
+        {
+            var basePath = Path.GetDirectoryName(assembly.Location);
+            if (basePath is null)
+                throw new InvalidOperationException("Failed to get assembly folder path!");
+
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .SetFileLoadExceptionHandler(ctx => ctx.Ignore = true)
+                .AddJsonFile($"appsettings.{assembly.GetNameWithoutVersion()}.json", true);
+
+            AddJSonFiles(configBuilder, basePath, additionalJsonFileNamePattern);
+
+            return configBuilder.Build();
+        }
+
+        /// <summary>
+        /// Returns the assembly name without the version number at the end.
+        /// </summary>
+        /// <param name="assembly">Assembly.</param>
+        public static string GetNameWithoutVersion(this Assembly assembly)
+        {
+            var assemblyName = assembly.GetName().Name;
+            var match = AssemblyNameVersionRex.Match(assemblyName);
+            return match.Success
+                ? assemblyName.Substring(0, assemblyName.Length - match.Groups["version"].Length)
+                : assemblyName;
+        }
+
+        private static void AddJSonFiles(IConfigurationBuilder configBuilder, string basePath, string? fileNamePattern)
+        {
+            if (string.IsNullOrEmpty(fileNamePattern))
+                return;
+
+            var additionalFilePaths =
+                Directory.GetFiles(basePath, fileNamePattern, SearchOption.AllDirectories);
+
+            foreach (var additionalFilePath in additionalFilePaths)
+            {
+                var jsonFile = PathUtils.GetRelativePath(basePath, additionalFilePath);
+                configBuilder.AddJsonFile(jsonFile, true);
+            }
+        }
+    }
+}
---
+++ src/Core/RxBim.Tools/Extensions/PathUtils.cs
@@ -0,0 +1,43 @@
+﻿namespace RxBim.Tools
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Path Utilities.
+    /// </summary>
+    public static class PathUtils
+    {
+        /// <summary>
+        /// Creates a relative path from one file or folder to another.
+        /// </summary>
+        /// <param name="fromPath">Contains the directory that defines the start of the relative path.</param>
+        /// <param name="toPath">Contains the path that defines the endpoint of the relative path.</param>
+        /// <returns>The relative path from the start directory to the end path.</returns>
+        public static string GetRelativePath(string fromPath, string toPath)
+        {
+            var fromUri = new Uri(AppendDirectorySeparatorChar(fromPath));
+            var toUri = new Uri(AppendDirectorySeparatorChar(toPath));
+
+            if (fromUri.Scheme != toUri.Scheme)
+                return toPath;
+
+            var relativeUri = fromUri.MakeRelativeUri(toUri);
+            var relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (string.Equals(toUri.Scheme, Uri.UriSchemeFile, StringComparison.OrdinalIgnoreCase))
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+
+            return relativePath;
+        }
+
+        private static string AppendDirectorySeparatorChar(string path)
+        {
+            // Append a slash only if the path is a directory and does not have a slash.
+            return !Path.HasExtension(path) &&
+                   !path.EndsWith(Path.DirectorySeparatorChar.ToString())
+                ? path + Path.DirectorySeparatorChar
+                : path;
+        }
+    }
+}
