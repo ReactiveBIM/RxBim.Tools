899536486f6b6fe90b61d8699b9c482a5b624e9c
Evgivachev <evgivachev@gmail.com>
2023-02-27 18:20:44 +0300

    reference equality comparer

--- src/Core/RxBim.Tools.TableBuilder/Helpers/ReferenceEqualityComparer.cs
+++ src/Core/RxBim.Tools.TableBuilder/Helpers/ReferenceEqualityComparer.cs
@@ -8,6 +8,12 @@ public class ReferenceEqualityComparer : EqualityComparer<object>
     /// <inheritdoc />
     public override bool Equals(object x, object y)
     {
+        var typeX = x.GetType();
+        var typeY = y.GetType();
+        if (typeX != typeY)
+            return false;
+        if (typeX.IsValueType || x is string)
+            return x.Equals(y);
         return ReferenceEquals(x, y);
     }
 
