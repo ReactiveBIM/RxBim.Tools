554d19ddf043830a54428f51f64251f22a5f7fc0
Snakye <gerasimovea@pik.ru>
2023-03-22 12:46:42 +0300

    add LogStorageTests

---
+++ tests/RxBim.Tools.Tests/Models/ObjectIdWrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace RxBim.Tools.Tests.Models;
+
+/// <summary>
+/// Wrapper for integer value.
+/// </summary>
+public class ObjectIdWrapper : Wrapper<int>, IObjectIdWrapper
+{
+    public ObjectIdWrapper(int wrappedObject) : base(wrappedObject)
+    {
+    }
+}
---
+++ tests/RxBim.Tools.Tests/TestDiConfigurator.cs
@@ -0,0 +1,13 @@
+﻿namespace RxBim.Tools.Tests
+{
+    using RxBim.Di;
+
+    public class TestDiConfigurator : DiConfigurator<IPluginConfiguration>
+    {
+        /// <inheritdoc />
+        protected override void ConfigureBaseDependencies()
+        {
+            Container.AddTransient<ILogStorage, LogStorage>();
+        }
+    }
+}
