5ba77157a785386610a1eeae30ba0648596102f6
Snakye <gerasimovea@pik.ru>
2023-03-22 13:05:55 +0300

    add Sample

---
+++ samples/RxBim.Tools.LogStorage.Sample/Models/ObjectIdWrapper.cs
@@ -0,0 +1,15 @@
+﻿namespace RxBim.Tools.LogStorage.Sample.Models;
+
+using RxBim.Tools;
+
+/// <summary>
+/// Wrapper for integer value.
+/// </summary>
+public class ObjectIdWrapper : Wrapper<int>, IObjectIdWrapper
+{
+    /// <inheritdoc />
+    public ObjectIdWrapper(int wrappedObject)
+        : base(wrappedObject)
+    {
+    }
+}
---
+++ samples/RxBim.Tools.LogStorage.Sample/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace RxBim.Tools.LogStorage.Sample;
+
+using Di;
+using Models;
+using Tools;
+
+/// <summary>
+/// Console application.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Main.
+    /// </summary>
+    public static void Main()
+    {
+        // Create a simple container and register the log storage.
+        var container = CreateContainer();
+
+        var logStorage = container.GetService<ILogStorage>();
+
+        // Add text message to storage
+        logStorage.AddMessage(new TextMessage("Test message"));
+
+        // For add text with id message you will need to create
+        // your own object inherited from IObjectIdWrapper interface
+        logStorage.AddMessage(new TextWithIdMessage("Test message", new ObjectIdWrapper(1)));
+
+        // Get messages count
+        logStorage.Count();
+    }
+
+    private static IContainer CreateContainer()
+    {
+        var container = new SimpleInjectorContainer();
+        container.AddSingleton<ILogStorage, LogStorage>();
+        return container;
+    }
+}
