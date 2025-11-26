fdcc37be415bff8f5eaf8d403ce7be6660ee48db
Вильдар Хисяметдинов <khisyametdinovvt@pik.ru>
2025-11-26 18:12:03 +0500

    closedxml 0.100.3

--- src/Excel/RxBim.Tools.TableBuilder.Excel/Converters/ExcelTableConverter.cs
+++ src/Excel/RxBim.Tools.TableBuilder.Excel/Converters/ExcelTableConverter.cs
@@ -127,7 +127,7 @@ internal class ExcelTableConverter : IExcelTableConverter
                 break;
 
             case NumericCellContent numeric:
-                cell.Value = XLCellValue.FromObject(numeric.ValueObject);
+                cell.Value = numeric.ValueObject.FromObject();
                 cell.Style.NumberFormat.Format = numeric.Format;
                 break;
 
@@ -136,7 +136,7 @@ internal class ExcelTableConverter : IExcelTableConverter
                 break;
 
             default:
-                cell.SetValue(content.ValueObject?.ToString());
+                cell.Value = content.ValueObject.FromObject();
                 break;
         }
     }
--- src/Excel/RxBim.Tools.TableBuilder.Excel/Extensions/XlCellExtensions.cs
+++ src/Excel/RxBim.Tools.TableBuilder.Excel/Extensions/XlCellExtensions.cs
@@ -23,4 +23,28 @@ public static class XlCellExtensions
         XLDataType.Blank => string.Empty,
         _ => cell.Value
     };
+
+    /// <summary>
+    /// Возвращает значение ячейки.
+    /// </summary>
+    /// <param name="value">Значение.</param>
+    public static XLCellValue FromObject(this object? value) => value switch
+    {
+        byte b => b,
+        sbyte sb => sb,
+        short s => s,
+        ushort us => us,
+        int i => i,
+        uint ui => ui,
+        long l => l,
+        ulong ul => ul,
+        float f => f,
+        double d => d,
+        decimal m => m,
+        bool bl => bl,
+        DateTime dt => dt,
+        TimeSpan ts => ts,
+        string s => s,
+        _ => Blank.Value
+    };
 }
